2ed32477454fe10909da490799f55e44e2839697
Davide Guglielmino <davideguglielmino.programmer@gmail.com>
2020-09-09 23:15:14 +0200

    Starting Project and Dice Table

---
+++ Source/Invader/Invader.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Invader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Invader, "Invader" );
+ 
---
+++ Source/Invader/InvaderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvaderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInvaderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInvaderGameMode();
+};
+
+
+
